3e0b95de1f26286fd402496df2be228d69ee20bf
ryanik <ryanik@amazon.com>
2019-05-28 23:18:49 -0700

    updated console module

--- labs/lab0/setup.sh
+++ labs/lab0/setup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-cd ~/openshift-on-aws/labs/lab0
-docker build -t registry registry/
-docker run --restart="always" --name registry -p 5000:5000 -d registry
-docker build -t mariadb mariadb/
-docker build -t wordpress wordpress/
-docker tag mariadb localhost:5000/mariadb
-docker tag wordpress localhost:5000/wordpress
-docker push localhost:5000/mariadb
-docker push localhost:5000/wordpress
-docker stop mariadb wordpress
+read -p "This script is to jump over the previous labs, is that really what you want to do? [y/N] " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo yum install -y ansible
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    ansible-playbook -i "localhost," -c local $DIR/jump-to-here-playbook.yml
+fi
